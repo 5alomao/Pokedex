70cce4e00e487a1e8fca3025ee3dceb5b6eabb0c
Salomão Ferreira Junior <salomao.junior2k22@gmail.com>
2022-11-01 21:47:35 -0300

    Modificações adicionadas (projeto em andamento)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,27 @@
 
-const offset = 0;
-const limit = 0;
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+function converPokemonTypesLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>` )
+}
 
-fetch(url)
-    .then((response) => (response.jason()))
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) =>  console.error(error))
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">${pokemon.order}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                ${converPokemonTypesLi(pokemon.types).join('')}
+            </ol>
+            <img src="${pokemon.sprites.other.dream_world.front_default}"
+                alt="${pokemon.name}">
+        </div>
+    </li >
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+})
